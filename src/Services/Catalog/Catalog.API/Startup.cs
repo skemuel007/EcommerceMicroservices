c49d927b9bacb1fe8f6dea0351737210df5a8021
Salvation Kemuel <skemuel007@gmail.com>
2022-02-22 17:06:44 +0100

    API versioning, swagger update, compression and global error handler

--- src/Services/Catalog/Catalog.API/Startup.cs
+++ src/Services/Catalog/Catalog.API/Startup.cs
@@ -1,11 +1,14 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,6 +18,7 @@ namespace Catalog.API
 {
     public class Startup
     {
+        readonly string AllowedOriginSpecifications = "AllowOrigin";
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -25,26 +29,119 @@ namespace Catalog.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // configure cors
+            services.AddCors(c =>
+            {
+                c.AddPolicy(name: AllowedOriginSpecifications, options => options
+                .SetIsOriginAllowed(_ => true)
+                .AllowAnyOrigin()
+                .AllowAnyHeader()
+                .AllowAnyMethod());
+            });
+
+            // add compression to response
+            services.AddResponseCompression();
+
+            // enable api health check
+            services.AddHealthChecks();
+            services.AddHttpContextAccessor();
+
+            services.AddMemoryCache(options =>
+            {
+                options.SizeLimit = 1024;
+            }
+            );
+
+            services.AddResponseCaching(options =>
+            {
+                // Each response cannot be more than 1 KB 
+                options.MaximumBodySize = 1024;
+
+                // Case Sensitive Paths 
+                // Responses to be returned only if case sensitive paths match
+                options.UseCaseSensitivePaths = true;
+            });
 
             services.AddControllers()
-                .AddNewtonsoftJson(options => options.UseMemberCasing()); ;
-            services.AddSwaggerGen(c =>
+                .AddNewtonsoftJson(options => {
+                    options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+                    options.UseMemberCasing();
+                });
+
+
+            // Configure Api version
+            services.AddApiVersioning(options =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Catalog.API", Version = "v1" });
+                options.DefaultApiVersion = new ApiVersion(1, 0);
+                options.AssumeDefaultVersionWhenUnspecified = true;
+                options.ReportApiVersions = true;
+                options.ApiVersionReader = ApiVersionReader.Combine(
+                    new QueryStringApiVersionReader("api-version"),
+                    new HeaderApiVersionReader("api-version")
+                   );
+            });
+
+            // versioning explorer
+            services.AddVersionedApiExplorer(options =>
+            {
+                // add the versioned api explorer, which also adds IApiVersionDescriptionProvider service
+                // note: the specified format code will format the version as "'v'major[.minor][-status]"
+                options.GroupNameFormat = "'v'VVV";
+
+                // note: this option is only necessary when versioning by url segment. the SubstitutionFormat
+                // can also be used to control the format of the API version in route templates
+                options.SubstituteApiVersionInUrl = true;
+            });
+
+            services.AddTransient<IConfigureOptions<SwaggerGenOptions>, ConfigureSwaggerOptions>();
+            services.AddSwaggerGen(options => options.OperationFilter<SwaggerDefaultValues>());
+
+            // format global json message response
+            services.Configure<ApiBehaviorOptions>(options =>
+            {
+                options.InvalidModelStateResponseFactory = actionContext =>
+                {
+                    var errors = actionContext.ModelState.Where(e =>
+                       e.Value.Errors.Count > 0
+                       ).Select(e => new
+                       {
+                           Error = e.Value.Errors.First().ErrorMessage,
+                       }).ToArray();
+
+                    return new BadRequestObjectResult(new
+                    {
+                        Status = false,
+                        Message = errors,
+                        Data = new { }
+                    });
+                };
             });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IApiVersionDescriptionProvider provider)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Catalog.API v1"));
+                app.UseSwaggerUI(
+                    options =>
+                    {
+                    // build a swagger endpoint for each discovered API version
+                    foreach (var description in provider.ApiVersionDescriptions)
+                        {
+                        //options.SwaggerEndpoint($"/swagger/{description.GroupName}/swagger.json", description.GroupName.ToUpperInvariant());
+                        options.SwaggerEndpoint($"{description.GroupName}/swagger.json", description.GroupName.ToUpperInvariant());
+                        }
+                    });
             }
 
             app.UseRouting();
+
+            // use cors
+            app.UseCors(AllowedOriginSpecifications);
+            app.UseResponseCaching();
 
             app.UseAuthorization();
 
